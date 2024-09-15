38a3ca00505575f7c46832e54ae36dab865b9ba0
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-09-16 02:49:07 +0530

    upate/hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import Image from "next/image";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="max-container padding-container flex flex-col gap-20  py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
 
-export default Hero
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">EcoVistara</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          Embark on a soulful journey with us, where the untouched beauty of
+          nature awaits your discovery. At Putuk Truno, we celebrate your spirit
+          of adventure and bring the grandeur of the world to your fingertips,
+          all in one app. Whether you're planning a trek through the Himalayas
+          or seeking the serene backwaters of Kerala, our app is your trusted
+          companion. With a deep respect for India's diverse landscapes and rich
+          cultural heritage, we are here to make your explorations
+          unforgettable. Let us be part of every step you take towards
+          experiencing nature's untainted glory, from the snow-capped peaks of
+          the north to the sun-kissed shores of the south. Adventure Awaits,
+          India!
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            45K
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button
+            type="button"
+            title="How We Work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </div>
+      </div>
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+
+
+          <div className="flex flex-col ">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Parvati Valley</p>
+          </div>
+
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">128.6 km</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">5.256km</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
