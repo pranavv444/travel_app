74b9dd08bb69f7f64fdc3fd40ba098ad363084c2
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-09-16 19:59:36 +0530

    fix/navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/EcoVistara1.jpg" alt="logo" width={150} height={29} />
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul className={`lg:flex hidden h-full gap-12 ${isMenuOpen ? "block" : "hidden"}`}>
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
@@ -23,19 +32,33 @@ const Navbar = () => {
 
       <div className="lg:flexCenter hidden">
         <Button
-        type="button"
-        title="Login"
-        icon="/user.svg"
-        variant="btn_dark_green"
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
         />
       </div>
       <Image
-      src="menu.svg"
-      alt="menu"
-      width={32}
-      height={32}
-      className="inline-block cursor-pointer lg:hidden  "
+        src="/menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
       />
+      {isMenuOpen && (
+        <ul className="absolute top-16 left-0 w-full bg-white flex flex-col items-center lg:hidden">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
